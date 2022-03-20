71bd5420de1ffb8a06bead202b16db70e2a46143
jamelsalah <57234834+jamelsalah@users.noreply.github.com>
2022-03-20 00:25:18 -0300

    listagem

--- App.tsx
+++ App.tsx
@@ -1,19 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { View, Text, FlatList } from 'react-native';
 import styled from 'styled-components/native'
+import MovieRow from './components/MovieRow'
+import api from './api'
 
 const StyledText = styled.Text`
   color: white;
+  font-size: 20px;
+  display: flex;
 `
 const StyledView = styled.View`
   backgroundColor: black;
 `
 
 export default function App() {
+  var [ movieList, setMovieList ] = useState([] as any)
+  const [featureData, setFeatureData] = useState(null)
+
+  useEffect(() => {
+    const loadAll = async () => {
+      const list = await api.getHomeList()
+      setMovieList(list)
+    }
+    loadAll();
+  }, [])
   return (
-    <StyledView>
-      <StyledText>Movie Stream</StyledText>
-    </StyledView>
+    <View style={{marginTop: 20, padding: 10}}>
+      <FlatList 
+          data={ movieList }
+          renderItem={ ({ item }) => (
+            <MovieRow title={item.title} items={item.items}></MovieRow>
+          )}
+          keyExtractor={ item => item.id } 
+        />
+    </View>
   );
 }
 
---
+++ api.ts
@@ -0,0 +1,56 @@
+const API_KEY = '120deccb8ea874887762e9e99cb4fbc2'
+const API_BASE = 'https://api.themoviedb.org/3'
+
+
+const basicFetch = async (endpoint: string) => {
+    var req: any
+    try {
+       req = await fetch(`${API_BASE}${endpoint}`);
+    } catch (err) {
+        if (err instanceof Error) {
+            console.log(err.message);
+          } else {
+            console.log('Unexpected error', err);
+          }
+    } 
+    const json = await req.json();
+    return json;
+}
+
+export default {
+    getHomeList: async() => {
+        return [
+            {
+                slug: 'toprated',
+                title: 'Filmes Populares',
+                items: await basicFetch(`/movie/top_rated?language=pt-br&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'releases',
+                title: 'Proximos Lançamentos',
+                items: await basicFetch(`/discover/movie?with_genres=new&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'trending',
+                title: 'Recomendados para você',
+                items: await basicFetch(`/trending/all/week?language=pt-BR&api_key=${API_KEY}`)
+            },
+        ];
+    },
+    // getMovieInfo: async (movieId, type) => {
+    //     let info = {};
+
+    //     if(movieId) {
+    //         switch(type) {
+    //             case 'movie':
+    //                 info = await basicFetch(`/movie/${movieId}?language=pt-BR&api_key=${API_KEY}`)
+    //             break;
+    //             case 'tv':
+    //                 info = await basicFetch(`/tv/${movieId}?language=pt-BR&api_key=${API_KEY}`)
+    //             break;
+    //         }
+    //     }
+
+    //     return info;
+    // }
+}
